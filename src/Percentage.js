ee14c952fd046753d82673f32e17c076248fd396
IvanR <ivan.ramirezf@gmail.com>
2024-11-21 22:32:20 +1000

    Percentage made controlled component

--- src/Percentage.js
+++ src/Percentage.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 function Percentage({ label }) {
+  const [percentage, setPercentage] = useState("");
+
   return (
     <div>
       <span>{label}</span>{" "}
       <span>
-        <select>
+        <select
+          value={percentage}
+          onChange={(e) => setPercentage(e.target.value)}
+        >
+          <option value="">Select an option</option>
           <option value="0">Disatisfied (0%)</option>
           <option value="5">It was ok (5%)</option>
           <option value="10">It was good (10%)</option>
